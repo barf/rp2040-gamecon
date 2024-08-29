9d4eebf23b7349fdfa4f3115ea2859acfd83bec7
Stuart MacIntosh <stuart@macintosh.nz>
2024-08-29 21:53:18 +1200

    read ADC2 (12 bit) and scale to uint8 with fixed floor/ceiling for simple potentiometer input test

--- src/gamecon.c
+++ src/gamecon.c
@@ -23,16 +23,30 @@
  *
  */
 
+// https://forums.raspberrypi.com/viewtopic.php?t=337719#p2025214
+#define NUM_FRAC    16
+#define FLOAT_TO_FIX_SCALE  65536.0 // 1<<16
+#define MULT_SHIFT  16
+#define FIX_TO_FLOAT    (1.0/65536.0)
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
+#include <stdarg.h>
 
 #include "bsp/board.h"
 #include "tusb.h"
 #include "pico/stdlib.h"
+#include "pico/printf.h"
+#include "pico/stdio/driver.h"
 #include "hardware/pwm.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
 void hid_task(void);
+char debug_buffer[80];
+uint16_t sma_buffer[1024];
 
 struct report
 {
@@ -43,25 +57,39 @@ struct report
     uint8_t joy3;
 } report;
 
+struct filter
+{
+    uint16_t floor;
+    uint16_t ceiling;
+} filter;
+
 int main(void)
 {
+    filter.floor = 65535;
+    filter.ceiling = 0;
 
     board_init();
-
     tusb_init();
 
-    gpio_set_function(PICO_DEFAULT_LED_PIN, GPIO_FUNC_PWM);
-    uint slice_num = pwm_gpio_to_slice_num(PICO_DEFAULT_LED_PIN);
+    adc_init();
+    adc_set_temp_sensor_enabled(false);
+    adc_gpio_init(26);
+    adc_gpio_init(27);
+    adc_gpio_init(28);
+    // adc_gpio_init(29);
 
-    pwm_clear_irq(slice_num);
-    pwm_set_irq_enabled(slice_num, false);
-    // Get some sensible defaults for the slice configuration. By default, the
-    // counter is allowed to wrap over its maximum range (0 to 2**16-1)
-    pwm_config config = pwm_get_default_config();
-    // Set divider, reduces counter clock to sysclock/this value
-    pwm_config_set_clkdiv(&config, 1.f);
-    // Load the configuration into our PWM slice, and set it running.
-    pwm_init(slice_num, &config, true);
+    uart_init(uart0, 115200);
+    gpio_set_function(0, UART_FUNCSEL_NUM(uart0, 0));
+    gpio_set_function(1, UART_FUNCSEL_NUM(uart0, 1));
+
+    stdio_init_all();
+    uart_puts(uart0, "INIT OK\r\n");
+
+#ifdef PICO_DEFAULT_LED_PIN
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
+#endif
 
     while (1)
     {
@@ -108,23 +136,102 @@ union
     uint8_t raw[25];
 } light_data;
 
+/* Choose 'C' for Celsius or 'F' for Fahrenheit. */
+#define TEMPERATURE_UNITS 'C'
+
+/* References for this implementation:
+ * raspberry-pi-pico-c-sdk.pdf, Section '4.1.1. hardware_adc'
+ * pico-examples/adc/adc_console/adc_console.c */
+float read_onboard_temperature(const char unit) {
+    
+    /* 12-bit conversion, assume max value == ADC_VREF == 3.3 V */
+    const float conversionFactor = 3.3f / (1 << 12);
+
+    float adc = (float)adc_read() * conversionFactor;
+    float tempC = 27.0f - (adc - 0.706f) / 0.001721f;
+
+    if (unit == 'C') {
+        return tempC;
+    } else if (unit == 'F') {
+        return tempC * 9 / 5 + 32;
+    }
+
+    return -1.0f;
+}
+
+// https://stackoverflow.com/a/29135850
+uint16_t roundup(uint16_t m, uint16_t n)
+{
+ return (m + n - 1) / n  ;
+}
+
+uint16_t rounddown(uint16_t m, uint16_t n)
+{
+ return m / n;
+}
+
+uint16_t round_uint16(uint16_t m, uint16_t n)
+{
+   int mod = m % n;
+
+   if(mod >= (n + 1) / 2)
+      return roundup(m, n);
+   else
+      return rounddown(m, n);
+}
+
+// gamecon.c
 void hid_task(void)
 {
+    const uint16_t h_floor = 1024;
+    const uint16_t h_ceiling = 2048;
+
     // Poll every 1ms
     const uint32_t interval_ms = 1;
     static uint32_t start_ms = 0;
+
+    // adc conversion factor: 3.3v ref
+    const float conversion_factor = 3.3f / (1 << 12);
 
     if (board_millis() - start_ms < interval_ms)
         return; // not enough time
     start_ms += interval_ms;
 
-    report.buttons = ((board_millis() / 1000) % 2) << ((board_millis() / 2000) % 16);
-    report.joy0 = light_data.lights.rgb[2].g;
-    report.joy1 = light_data.lights.rgb[2].b;
-    report.joy2 = ((board_millis() / 1000) % 4) * 64;
-    report.joy3 = board_millis() / 2;
-    int fade = report.joy1;
-    pwm_set_gpio_level(PICO_DEFAULT_LED_PIN, fade * fade);
+    // Select ADC input 0 (GPIO26)
+    adc_select_input(0);
+    uint16_t result_ADC0 = adc_read();
+    adc_select_input(1);
+    uint16_t result_ADC1 = adc_read();
+    adc_select_input(2);
+    uint16_t result_ADC2 = adc_read();
+
+    // auto scaling?
+    if (result_ADC2 < filter.floor) {
+        // 0x431 // 1073
+        filter.floor = result_ADC2;
+    }
+    if (result_ADC2 > filter.ceiling) {
+        // 0x7c1 // 1985
+        filter.ceiling = result_ADC2;
+    }
+
+    // clamping
+    uint16_t axis_0;
+    if (result_ADC2 < h_floor) {
+        result_ADC2 = h_floor;
+    } else if (result_ADC2 > h_ceiling) {
+        result_ADC2 = h_ceiling;
+    } else {
+        // scale and range to uint8
+        // uint16_t axis_0 = (result_ADC2 - filter.floor) / (filter.ceiling/256); // truncates
+        axis_0 = round_uint16((result_ADC2 - h_floor), (h_ceiling/256));
+    }
+
+    report.buttons = 0;
+    report.joy0 = (uint8_t)axis_0;
+    report.joy1 = (uint8_t)filter.floor;
+    report.joy2 = (uint8_t)filter.ceiling;
+    report.joy3 = 0;
 
     // Remote wakeup
     if (tud_suspended())
@@ -138,6 +245,10 @@ void hid_task(void)
     {
         tud_hid_n_report(0x00, 0x01, &report, sizeof(report));
     }
+
+    // debug strings on UART0
+    // snprintf(debug_buffer, sizeof debug_buffer, "ADC0:0x%03x\tADC1:0x%03x\tADC2:0x%03x\r\n", result_ADC0, result_ADC1, result_ADC2);
+    // uart_puts(uart0, debug_buffer);
 }
 
 //--------------------------------------------------------------------+
